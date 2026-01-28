54816ddfbaf1ed3c95a70bdc4bdbe96ca4b48621
zhangjw1 <31002201+zhangjw1@users.noreply.github.com>
2026-01-28 18:01:09 +0800

    Add main.go

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "etherum-monitor/wallet"
+
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func main() {
+	wallet.AddressAddMonitor()
+}
