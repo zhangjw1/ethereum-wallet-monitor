190dd83bee9e6489e880b503aec6f329bfbacae8
zhangjw1 <31002201+zhangjw1@users.noreply.github.com>
2026-01-28 18:01:17 +0800

    Add config/MonitorConfig.go

---
+++ config/MonitorConfig.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	ETHEREUM_RPC_URL   = "https://mainnet.infura.io/v3/YOUR_INFURA_KEY"
+	OKX_WALLET_ADDRESS = "0x6ea08ca8f313d860808ef7431fc72c6fbcf4a72d"
+	SLEEP_SECONDS_FOR_NEW_BLOCK = 10
+	ETH_THRESHOLD               = 10
+)
